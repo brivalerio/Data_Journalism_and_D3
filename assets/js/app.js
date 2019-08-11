be9020453a9730c72d6b2d55e4f8f0c094a43493
brivalerio <brivalerio@gmail.com>
2019-08-10 21:47:24 -0700

    saving progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,9 +23,6 @@ var svg = d3
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Initial Params
-var chosenXAxis = "age"
-var chosenYAxis = "smokes"
 
 // Retrieve data from the CSV file and execute everything below
 d3.csv("assets/data/data.csv")
@@ -39,58 +36,43 @@ d3.csv("assets/data/data.csv")
     });
 
     //xLinearScale
-    // var xLinearScale = d3.scaleLinear()
-    //     .domain([d3.max(stateHealth, d => d.age)])
-    //     .range([0, width]);
-    var xLinearScale = xScale(stateHealth, chosenXAxis);
+    var xLinearScale = d3.scaleLinear()
+        .domain([28,d3.max(stateHealth, d => d.age)+1])
+        .range([0, width]);
 
-    // y scale function
-    // var yLinearScale = d3.scaleLinear()
-    //     .domain([0,d3.max(stateHealth, d => d.smokes)])
-    //     .range([height, 0]);
-    var yLinearScale = yScale(stateHealth, chosenYAxis);
+    // yLinearScale
+    var yLinearScale = d3.scaleLinear()
+        .domain([8,d3.max(stateHealth, d => d.smokes)+1])
+        .range([height, 0]);
 
     // Initial axis
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
     // append x axis
-    var xAxis = chartGroup.append("g")
-        .classed("x-axis", true)
+    chartGroup.append("g")
+        
         .attr("transform", `translate(0, ${height})`)
         .call(bottomAxis);
 
     // append y axis
-    var yAxis = chartGroup.append("g")
-        .classed("y-axis", true)
+    chartGroup.append("g")
         .call(leftAxis);
 
-  // append initial circles
+    // append circles
     var circlesGroup = chartGroup.selectAll("circle")
         .data(stateHealth)
         .enter()
         .append("circle")
-        .attr("cx", d => xLinearScale(d[chosenXAxis]))
+        .attr("cx", d => xLinearScale(d.age))
         .attr("cy", d => yLinearScale(d.smokes))
         .attr("r", 20)
         .attr("fill", "pink")
         .attr("opacity", ".5");
 
-    // x label
-    var aLabel = xlabelsGroup.append("text")
-        .attr("x",0)
-        .attr("y",40)
-        .attr("value", "age")
-        // .classed("active", true)
-        .text("Age (Median)");
+    // initialize tool tip
 
-    // y label
-    var sLabel = ylabelsGroup.append("text")
-        .attr("x",0)
-        .attr("y",-10)
-        .attr("value", "smokes")
-        // .classed("active", true)
-        .text("Smokes (%)");    
+
 
 
 
