5043911cc59c56e73aa8b6d77653c14a67fba83a
brivalerio <brivalerio@gmail.com>
2019-08-10 21:05:15 -0700

    saving progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,7 @@ var height = svgHeight - margin.top - margin.bottom;
 // Create an SVG wrapper, append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
 var svg = d3
-  .select(".chart")
+  .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -25,29 +25,73 @@ var chartGroup = svg.append("g")
 
 // Initial Params
 var chosenXAxis = "age"
+var chosenYAxis = "smokes"
 
 // Retrieve data from the CSV file and execute everything below
-d3.csv("assets/data/data.csv", function(stateHealth){
-    console.log(stateHealth);
+d3.csv("assets/data/data.csv")
+    .then(function(stateHealth){
 
     // parse through data
     stateHealth.forEach(function(data){
-        data.smokes = +data.smokes;
         data.age = +data.age;
-    })
+        data.smokes = +data.smokes;
+   
+    });
 
     //xLinearScale
-    var xLinearScale = xScale(stateHealth, chosenXAxis);
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.max(stateHealth, d => d.age)])
+        .range([0, width]);
+    // var xLinearScale = xScale(stateHealth, chosenXAxis);
 
     // y scale function
     var yLinearScale = d3.scaleLinear()
         .domain([0,d3.max(stateHealth, d => d.smokes)])
         .range([height, 0]);
+    // var yLinearScale = yScale(stateHealth, chosenYAxis);
 
-    // Initial axis funcs
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.leftAxis(yLinearScale);
+    // Initial axis
+    var bottomAxis = d3.axisBottom(xLinearScale).ticks(7);
+    var leftAxis = d3.axisLeft(yLinearScale);
 
     // append x axis
-    
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    // append y axis
+    var yAxis = chartGroup.append("g")
+        .classed("y-axis", true)
+        .call(leftAxis);
+
+  // append initial circles
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(stateHealth)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d[chosenXAxis]))
+        .attr("cy", d => yLinearScale(d.smokes))
+        .attr("r", 20)
+        .attr("fill", "pink")
+        .attr("opacity", ".5");
+
+    // x label
+    var aLabel = xlabelsGroup.append("text")
+        .attr("x",0)
+        .attr("y",40)
+        .attr("value", "age")
+        // .classed("active", true)
+        .text("Age (Median)");
+
+    // y label
+    var sLabel = ylabelsGroup.append("text")
+        .attr("x",0)
+        .attr("y",-10)
+        .attr("value", "smokes")
+        // .classed("active", true)
+        .text("Smokes (%)");    
+
+
+
 })
